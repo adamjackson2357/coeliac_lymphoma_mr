4273a5d3067c6c1ef91bfdf8b7e9073afcecf8de
Adam Jackson <aj1520@login-4.rcs.ic.ac.uk>
2021-02-10 15:53:25 +0000

    Updated covariate extraction script

--- r_code/covariate_extraction.R
+++ r_code/covariate_extraction.R
@@ -1,33 +1,24 @@
-# As the entire UK Biobank dataset is quite big, it can be useful to create a copy with only the covariates of interest
-# (i.e. discarding any irrelevant variable for your analyses)
-# The code below is creating a copy containing only the variables of interest as defined in
-# a list of field IDs ("List_field_ids_to_extract.txt")
-# The field IDs of variables can be found on the UK Biobank website (in the data showcase)
-# Note: You need to change the path where the data is copied
+# Extract the covariates data
+
+# Clear variables and set the path
+dev.off()
+rm(list=ls())
+path=dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(path)
 
 library(data.table)
+source("extraction.R")
 
-# Loading the data
-mydata=data.frame(fread("../data/ukb26390.csv", nrows=5))
-myfields=unname(unlist(read.table("../configs/field_ids.txt", header=FALSE)))
+# read in the config
+config <- read_yaml('../configs/main.yml')
 
-# Extracting the column ids 
-column_id=grep("eid", colnames(mydata))
-found_fieldids=NULL
-for (k in 1:length(myfields)){
-  mygrep=grep(paste0("X",myfields[k],"."), fixed=TRUE, colnames(mydata))
-  if (length(mygrep)>0){
-    found_fieldids=c(found_fieldids, myfields[k])
-  }
-  column_id=c(column_id, mygrep)
-}
+# get the fields
+fields <- unname(unlist(read.table(config$fields_fname, header=FALSE)))
 
-# Extracting required columns from dataset
-covariates=data.frame(fread("../data/ukb26390.csv", select=column_id))
-
-# remove participants which have withdrawn
-withdrawn=read.csv("../data/w19266_20200204.csv")[,1]
-covariates <- subset(covariates, !(eid %in% withdrawn))
+# get the covariates
+covariates <- get_covariates(config$covariates_fname,
+                             config$withdrawn_fname,
+                             fields)
 
 # save
-saveRDS(covariates, "../data/covariates.rds")
+saveRDS(covariates, config$covariates_output)
